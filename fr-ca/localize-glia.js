c2b3851cc98ce430f5e5c76622679e459fa24aae
Scott Horton <103960849+sHortCodeRepo@users.noreply.github.com>
2025-07-14 12:06:10 -0700

    Create localize-glia.js

---
+++ fr-ca/localize-glia.js
@@ -0,0 +1,17 @@
+// This function sets the Glia locale to Canadian French
+function setGliaLocaleFrenchCA() {
+  // Check if the 'sm' object is available
+  if (typeof sm !== 'undefined' && sm.getApi) {
+    sm.getApi({version: 'v1'}).then(function(api){
+      api.setLocale('fr-CA');
+      console.log("Glia locale set to fr-CA.");
+    });
+  } else {
+    console.log("Glia API not ready, will try again.");
+    // Optional: you could add a retry mechanism here
+  }
+}
+
+// Attempt to set the locale once the main Glia script has likely loaded
+// A simple timeout can work for this demonstration
+setTimeout(setGliaLocaleFrenchCA, 2000); // Wait 2 seconds
